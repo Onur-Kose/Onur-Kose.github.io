e6f9367eb46f63bca9e1ab68a95494f786e6a2fb
Onur Köse <103755656+Onur-Kose@users.noreply.github.com>
2024-04-08 21:18:33 +0300

    mail servisleri düzenlendi

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+  window.onload = function() {
+    var url = window.location.href;
+    var queryString = url.substring(url.indexOf("?") + 1);
+    var messageText = queryString ? decodeURIComponent(queryString) : "";
+    var messageTextArea = document.getElementById('message');
+    var subject = document.getElementById('subject');
+    subject.value = "Ayrıntılı bilgi"
+    messageTextArea.value = messageText + "\" projeniz hakkında ayrıntılı bilgi alabilir miyim?";
+}
